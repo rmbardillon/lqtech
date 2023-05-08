8f1e92cae2f6f92ead84eef4876efa98ef9d2bd9
Romeo Jr Montealegre Bardillon <107736688+rmbardillon@users.noreply.github.com>
2023-05-08 11:12:49 +0800

    Update db and card in home

--- views/master-page/home.php
+++ views/master-page/home.php
@@ -9,20 +9,34 @@
     <?php include_once("../layout/nav.php") ?>
     <div class="container mt-5">
         <div class="row">
-            <div class="col d-flex justify-content-center align-items-center" style="position: relative; height:400px; width:800px">
+            <!-- <div class="col d-flex justify-content-center align-items-center" style="position: relative; height:400px; width:800px">
                 <canvas id="transactionIn"></canvas>
+            </div> -->
+            <div class="card col mb-2 ms-2">
+                <div class="card-body">
+                    <div class="d-flex justify-content-center align-items-center" style="position: relative; height:400px; width:800px">
+                        <canvas id="transactionIn"></canvas>
+                    </div>
+                </div>
             </div>
-            <div class="col d-flex justify-content-center align-items-center" style="position: relative; height:400px; width:800px">
-                <canvas id="transactionOut"></canvas>
+            <div class="card col mb-2 ms-2">
+                <div class="card-body">
+                    <div class="d-flex justify-content-center align-items-center" style="position: relative; height:400px; width:800px">
+                        <canvas id="transactionOut"></canvas>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col d-flex justify-content-center align-items-center" style="position: relative; height:400px; width:800px">
-                <canvas id="productChart"></canvas>
+        <div class="row text-center">
+            <div class="col-3"></div>
+            <div class="card col-6">
+                <div class="card-body">
+                    <div class="d-flex justify-content-center align-items-center" style="position: relative; height:400px; width:800px">
+                        <canvas id="productChart"></canvas>
+                    </div>
+                </div>
             </div>
-            <!-- <div class="col d-flex justify-content-center align-items-center" style="position: relative; height:400px; width:800px">
-                <canvas id="chart"></canvas>
-            </div> -->
+            <div class="col-3"></div>
         </div>
     </div>
 <?php include_once("../layout/scripts.php") ?>
